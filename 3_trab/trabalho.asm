8d0e3a073f8b7baecd4322f15d910aee279fd0ce
Guilherme Sant'Ana <guilherme.sant_ana@outlook.com>
2018-05-24 13:50:29 -0300

    Correct support for double precision
    Some values were entering in a loop because the result will never
    converged. Now if the error is less tha 10^-13 the program will end

--- 3_trab/trabalho.asm
+++ 3_trab/trabalho.asm
@@ -5,58 +5,55 @@
 	str1: .asciiz "A raiz cubica eh "
 	str2: .asciiz ". O erro eh menor que "
 	newLine: .asciiz "\n"
-
+	errorLimit: .double 0.0000000000001
 	
-	# The parameters registers are f0 and f7 (replacing a0 and a1 from integer operations)
+	# The parameters registers are f0 and f14 (replacing a0 and a1 from integer operations)
 	# f0 also works as a return parameter
-	# f1 the number entered by the user
-	# f2 guess for NewtonRapson method
-	# f3 has the value of guess/last result power 3 and the value of the divident: (x0^3 - targetValue)
+	# f2 the number entered by the user
+	# f4 guess for NewtonRapson method
+	# f6 has the value of guess/last result power 3 and the value of the divident: (x0^3 - targetValue)
 	# and at last f3 also have the result of the division
-	# f4 stores the value 3, originated from the derivative
-	# f5 has the divider value : (x0^2 * 3)
-	# Double parse
-	# f1 = f2
-	# f2 = f4
-	# f3 = f6
-	# f4 = f8
-	# f5 = f10
-	# f7 = f14
+	# f8 stores the value 3, originated from the derivative
+	# f10 has the divider value : (x0^2 * 3)
 
 .text
 	jal ReadNumbers
-	mov.d $f2,$f0 # $f2 has the target value F1 -> F2
+	mov.d $f2,$f0 # $f2 has the target(user input) value
 	
-	l.d $f4,guess # f2 = f4
-	l.d $f8,three # f4 = f8
+	l.d $f4,guess 
+	l.d $f8,three
+	l.d $f16,errorLimit
 	
 	Newton_Raphson:
-		mov.d $f14,$f4 # f2 = f4 # f7 = f14
-		mov.d $f0,$f4 # f2 = f4
-		li $a0,3
-		jal Power
+		mov.d $f14,$f4 # make guess power 3
+		mov.d $f0,$f4  #
+		li $a0,3       #
+		jal Power      # stores the value in f0
 		
-		sub.d $f6,$f0,$f2 # f0 has the guess/last value power 3 F1 -> F2 # f3 = f6
+		sub.d $f6,$f0,$f2 # Dividend. subtract guess^3 by user input (x0^3 - input)
 		
-		mov.d $f14,$f4 # f2 = f4 # f7 = f14
-		mov.d $f0,$f4 # f2 = f4
-		li $a0,2
-		jal Power
+		mov.d $f14,$f4 # make guess power 2
+		mov.d $f0,$f4  #
+		li $a0,2	   #
+		jal Power      # stores the value in f0
 		
-		mul.d $f10,$f0,$f8 # Divider # f4 = f8 # f5 = f10
+		mul.d $f10,$f0,$f8 # Divider (3 * guess^2)
 		
+		div.d $f6,$f6,$f10 # Division result  (x0³ - input) / (3 - guess²)
 		
-		div.d $f6,$f6,$f10 # final result  # f3 = f6 # f5 = f10
+		sub.d $f6,$f4,$f6 # Subtract guess from division result
 		
-		sub.d $f6,$f4,$f6 # f2 = f4  # f3 = f6
+		jal Error # Calculate error
 		
-		c.eq.d $f4,$f6 # f2 = f4  # f3 = f6
-		bc1t Error
+		c.lt.d $f14,$f16 # Set True if error is less than e^-13
+		
+		bc1t PrintFinalMessage
+		
 		mov.d $f4,$f6 # f2 = f4  # f3 = f6
 		j Newton_Raphson
 	
-	Power: # $f0 and $f7 need to be loaded with the parameter
-		mul.d $f0,$f0,$f14 # f7 keeps the original value and f0 works as a accumulator # f7 = f14
+	Power: # $f0 and $f14 need to be loaded with the parameter
+		mul.d $f0,$f0,$f14 # f14 keeps the original value and f0 works as a accumulator
 		sub $a0,$a0,1     # subtracts one from a0(exponent)
 		bne $a0,1,Power   # when a0 has the value of 1 go back to caller
 		jr $ra
@@ -67,12 +64,21 @@
 		jr $ra	
 	
 	Error:
-		mov.d $f14,$f6	# performs f3^3(f3 being the result) and keeps the value in f0 # f3 = f6 # f7 = f14
-		mov.d $f0,$f6 # f3 = f6
+		mov.d $f14,$f6	# performs f6³(f6 being the result) and keeps the value in f0
+		mov.d $f0,$f6 
 		li $a0,3
+		
+		subi $sp,$sp,4 # store last $ra
+		sw $ra,0($sp)  #
+		
 		jal Power
 		
+		lw $ra,0($sp)  # load $ra stored
+		addi $sp,$sp,4 #
+		
 		sub.d $f14,$f2,$f0	# calculate error and save on f14
+		abs.d $f14,$f14		# get absolute value of the error
+		jr $ra
 		
 	PrintFinalMessage:
 		li $v0,4
